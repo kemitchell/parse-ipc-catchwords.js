773b9f98df057bad2a524266d65c204cc655ffe4
Kyle E. Mitchell <kyle@kemitchell.com>
2017-07-11 11:22:10 -0700

    Eat premature end errors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var Parser = require('node-expat').Parser
-var pump = require('pump')
 
 module.exports = function (stream, callback) {
   var data = {}
   var currentElement = null
   var currentIndication = null
   var parser = new Parser('UTF-8')
+    .on('error', function () {
+      console.error('got')
+      console.error(error)
+    })
     .on('startElement', function (name, attributes) {
       currentElement = name
       if (name === 'sref') {
@@ -23,12 +26,8 @@ module.exports = function (stream, callback) {
         }
       }
     })
-  pump(stream, parser, function (error) {
-    if (error) {
-      console.error(error)
-      callback(error)
-    } else {
+    .once('end', function () {
       callback(null, data)
-    }
-  })
+    })
+  stream.pipe(parser)
 }
